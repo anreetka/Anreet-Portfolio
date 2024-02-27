09d5515c4ec685016d62f5971793d794837ad8e2
Anreet Kaur <anreet.kaur@sheridancollege.ca>
2024-02-27 17:03:52 -0500

    added scroller and positionig to front page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import {motion} from 'framer-motion';
 import { styles } from '../styles';
 import { ComputersCanvas } from './canvas';
+
 const Hero = () => {
   return (
     <section className='relative w-full h-screen
@@ -18,6 +19,24 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-white flex justify-center items-start p-2'>
+            <motion.dev
+            animate={{
+              y: [0, 24, 0]
+            }}
+            transition={{
+              duration: 1.5,
+              repeat: Infinity,
+              repeatType: 'loop'
+            }}
+            className='w-3 h-3 rounded-full bg-white mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
