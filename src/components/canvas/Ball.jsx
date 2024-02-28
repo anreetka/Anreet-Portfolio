9bdb00be0ed5fe0cd6805ac90524459768d0f759
Anreet Kaur <anreet.kaur@sheridancollege.ca>
2024-02-27 21:29:10 -0500

    added work experience section

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {Suspense} from 'react'
+import { Canvas } from '@react-three/fiber';
+import {
+  Decal, Float, OrbitControls, Preload, useTexture
+} from '@react-three/drei';
+import {CanvasLoader} from '../Loader';
 
-const Ball = () => {
+const Ball = (props) => {
   return (
     <div>Ball</div>
   )
